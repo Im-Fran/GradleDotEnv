c3d9bcb1e281e490a5f5545429b63392c61b9c67
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2023-12-21 23:19:42 -0300

    patch: fixes and improvements to the extension (#2)
    * Now we're using DotEnvPluginExtension as custom class to provide better completion support.
    * Support from java 11 and higher.
    * Now we also support empty and quoted values

--- plugin/src/main/kotlin/cl/franciscosolis/gradledotenv/DotEnv.kt
+++ plugin/src/main/kotlin/cl/franciscosolis/gradledotenv/DotEnv.kt
@@ -1,32 +1,45 @@
 package cl.franciscosolis.gradledotenv
 
+import org.gradle.api.Project
 import java.io.File
 import java.nio.charset.Charset
 
-class DotEnv {
+class DotEnv(project: Project) {
 
     private val variables = mutableMapOf<String, String>()
 
     init {
-        val environment = System.getenv("ENV") ?: System.getProperty("ENV")
-        val file = File(".env.${environment?.lowercase() ?: ""}").let {
-            if (it.exists()) {
-                it
-            } else {
-                File(".env")
-            }
-        }
-
-        if (file.exists()) {
-            file.readLines(Charset.defaultCharset())
-                .filter { it.isNotBlank() && !it.startsWith(';') && !it.startsWith('#') }.associateTo(variables) {
-                val (key, value) = it.split("=")
-                key to value
-            }
+        val envFile = File(project.rootDir, determineSystemEnvFile())
+        if(envFile.exists()) {
+            loadEnvFile(envFile)
         }
 
         variables.putAll(System.getenv())
     }
 
-    fun environments(): Map<String, String> = variables.toMap()
+    private fun loadEnvFile(file: File) {
+        file.readLines(Charset.defaultCharset())
+            .filter { it.isNotBlank() && !it.startsWith(';') && !it.startsWith('#') }
+            .forEach { line ->
+                val keyValue = line.split("=", limit = 2)
+                if (keyValue.size == 2) {
+                    val (key, value) = keyValue
+                    variables[key.trim()] = removeQuotes(value.trim())
+                } else if (keyValue.size == 1) {
+                    variables[keyValue[0].trim()] = ""
+                }
+            }
+    }
+
+    private fun removeQuotes(value: String): String = if (value.startsWith('"') && value.endsWith('"')) {
+        value.substring(1, value.length - 1)
+    } else {
+        value
+    }
+
+    private fun determineSystemEnvFile(): String = (System.getenv("ENV") ?: System.getProperty("ENV"))?.let {
+        ".env.${it.lowercase()}"
+    } ?: ".env"
+
+    fun get(key: String): String? = variables[key]
 }
--- plugin/src/main/kotlin/cl/franciscosolis/gradledotenv/GradleDotEnvPlugin.kt
+++ plugin/src/main/kotlin/cl/franciscosolis/gradledotenv/GradleDotEnvPlugin.kt
@@ -3,13 +3,14 @@ package cl.franciscosolis.gradledotenv
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 
-class GradleDotEnvPlugin: Plugin<Project> {
-
+class GradleDotEnvPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        // Read the all env variables from the '.env' file at the root of the project and from the system environment variables.
-        val env = DotEnv()
+        val env = DotEnv(project)
 
-        // Add the 'env' extension to the project.
-        project.extensions.add("env", env.environments())
+        project.extensions.create("env", DotEnvPluginExtension::class.java, env)
     }
+}
+
+open class DotEnvPluginExtension(private val env: DotEnv) {
+    operator fun get(key: String): String? = env.get(key)
 }
